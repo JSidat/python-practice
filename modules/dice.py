8d2a3931c620bf15cbf3ff25ad80e5f138ab63a8
JSidat <JSidat@qa.com>
2020-05-04 16:04:11 +0100

    add modules files

---
+++ modules/dice.py
@@ -0,0 +1,7 @@
+from random import randint
+
+
+def random_throw():
+    value = randint(1, 6)
+    return value
+
---
+++ modules/dice_test.py
@@ -0,0 +1,7 @@
+import dice
+
+dice1 = dice.random_throw()
+dice2 = dice.random_throw()
+
+print(dice1)
+print(dice2)

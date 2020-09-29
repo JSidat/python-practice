7466c6744cb1cf96eddcd1f510660013d6aad994
JSidat <JSidat95@gmail.com>
2020-09-29 23:44:37 +0100

    add python file

--- python_exercises/Code/challenge_of_the_day6.py
+++ python_exercises/Code/challenge_of_the_day6.py
@@ -3,7 +3,7 @@ import random
 def five(x, y):
     new_list = []
     for i in range(5):
-        i = random.randrange(x, y, 2)
+        i = random.randrange(x, y+1, 2)
         new_list.append(i)
     return new_list
 
